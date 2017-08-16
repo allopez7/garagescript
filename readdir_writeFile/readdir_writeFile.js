9f032be757a299e84692ecb642231031ed46fe28
Alberto Lopez <alopez@llip.life>
2017-08-15 17:38:58 -0700

    Lesson 2: readdir and writeFile

---
+++ readdir_writeFile/readdir_writeFile.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+let namesString = "";
+const excludeMyName = (name) =>{
+  if(name != 'alopez'){
+    namesString=namesString+' '+name+'\n';
+  }
+}
+
+const allNames = (err, files) =>{
+  files.forEach(excludeMyName);
+  fs.writeFile('/home/alopez/garagescript/readdir/book.txt',namesString,(err) =>{});
+}
+
+fs.readdir('/home',allNames);
