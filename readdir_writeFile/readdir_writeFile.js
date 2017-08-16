9705919b9165b0add74d6c9c425632060dd50044
Alberto Lopez <alopez@llip.life>
2017-08-15 18:25:40 -0700

    Lesson: readdir_writeFile

--- readdir_writeFile/readdir_writeFile.js
+++ readdir_writeFile/readdir_writeFile.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 let namesString = "";
+
 const excludeMyName = (name) =>{
   if(name != 'alopez'){
     namesString=namesString+' '+name+'\n';
@@ -9,7 +10,7 @@ const excludeMyName = (name) =>{
 
 const allNames = (err, files) =>{
   files.forEach(excludeMyName);
-  fs.writeFile('/home/alopez/garagescript/readdir/book.txt',namesString,(err) =>{});
+  fs.writeFile('/home/alopez/garagescript/readdir_writeFile/book.txt',namesString,(err) =>{});
 }
 
 fs.readdir('/home',allNames);
