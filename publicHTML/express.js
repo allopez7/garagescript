edde860dc57766621a78f56ca8061853a2712676
Alberto Lopez <alopez@llip.life>
2017-08-16 16:05:48 -0700

    Lesson: publicHTML/writeFile

--- publicHTML/express.js
+++ publicHTML/express.js
@@ -1,13 +1,15 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 app.listen(3608);
-
 app.use(express.static('public'));
 
 const response = (req,res) =>{
+
+  fs.writeFile('/home/alopez/garagescript/publicHTML/public/book.txt',req.query.Name+': '+req.query.Comment+'\n',
+    (err)=>{});
   res.send('done');
 
 }
 
-app.get('/rock',response);
+app.get('/send',response);
