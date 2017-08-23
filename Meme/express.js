de71ace01759701690be340aa52198c724984f7f
Alberto Lopez <alopez@llip.life>
2017-08-23 14:50:16 -0700

    Lesson: Meme

---
+++ Meme/express.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const express = require('express');
+const bp = require('body-parser');
+const app = express();
+
+app.listen(3608);
+app.use(express.static('public'));
+app.use(bp.json({limit: "500mb"}));
+
+const response = (req,res) =>{
+  fs.appendFile('/home/alopez/garagescript/Webcam/public/book.txt',`${req.body.name}: ${req.body.comment}`+'\n',()=>{});
+  res.send();
+}
+
+app.post('/send', response);
+
+const resp2 = (req, res) =>{
+  fs.writeFile('/home/alopez/garagescript/Webcam/public/'+req.body.name+'.'+'png',req.body.Canvas.replace('data:image/png;base64',""),'base64',() =>{
+    gm('/home/alopez/texting_interval2/public/'+req.body.name+'.'+'png').fontSize(40).drawText(50,50,'hello').write('/home/alopez/texting_interval2/public/text.png',(err)=>{$
+        console.log("drror is", err);$
+        });
+});
+  res.send('done');  
+
+}
+
+app.post('/image', resp2);
