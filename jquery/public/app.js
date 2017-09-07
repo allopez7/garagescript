3c631165fd589e66c5da4c92059715cf1809efb1
Alberto Lopez <alopez@llip.life>
2017-09-07 14:00:48 -0700

    Lesson jquery

---
+++ jquery/public/app.js
@@ -0,0 +1,56 @@
+$('#submit').click(()=>{
+  const name = $('#name').val();
+  const comment = $('#comment').val();
+
+  $.ajax({
+    type: 'POST',
+    url: '/send',
+    contentType: 'application/json',
+    data: JSON.stringify(
+      {name: name,comment: comment}
+    ),
+    success: (data)=>{
+      console.log('Success Text: ', data);
+    }
+  });
+
+});
+//Display text on browser
+const getTextFile = () =>{
+  $.get('/book.txt',(data) =>{
+    const display = $('#browserViewText');     
+    display.html(data);
+    }
+  );
+}
+
+window.setInterval(getTextFile, 1000);
+
+//video 
+const video = $('#video')[0];
+navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream)=>{
+  video.srcObject = stream;
+  video.play();
+});
+
+//Take pictures 
+$('#picture').click(()=>{
+  const name = $('#name').val();
+  const comment = $('#comment').val();
+  const canvas = $('#canvas')[0];
+  const context = canvas.getContext('2d');
+  context.drawImage(video,0,0);
+  const dataURL = canvas.toDataURL('image/png');
+
+  $.ajax({
+    type: 'POST',
+    url: '/image',
+    contentType: 'application/json',
+    data: JSON.stringify(
+      {Canvas: dataURL,name: name,comment: comment}
+    ),
+    success: ()=>{
+      console.log('Success Image Taken')
+    }
+  });
+});
