595378771522f2640bf210055ac824bc11fced5b
Alberto Lopez <alopez@llip.life>
2017-09-06 18:52:50 -0700

    Lesson: jquery_challenge_final

--- jquery_challenge_final/express.js
+++ jquery_challenge_final/express.js
@@ -10,17 +10,7 @@ app.listen(3608);
 app.use(express.static('public'));
 app.use(bp.json({limit: "500mb"}));
 
-const appendTextToFile = (req,res) =>{
-  const textFilePath = '/home/alopez/garagescript/jquery_challenge_base64/public/book.txt'; 
-  const chatMessage = `${req.body.name}: ${req.body.comment}\n`; 
-
-  fs.appendFile(textFilePath, chatMessage);
-  res.send('append comment to /book.txt');
-}
-app.post('/send',appendTextToFile);
-
 const createImageMeme = (req, res) =>{
-
   const imageData = req.body.Canvas.replace('data:image/png;base64',""); 
   const imageMeme = `/home/alopez/garagescript/jquery_challenge_base64/public/images/${req.body.name}.png`; 
 
--- jquery_challenge_final/public/app.js
+++ jquery_challenge_final/public/app.js
@@ -1,37 +1,5 @@
-$('#submit').click(()=>{
-  const name = $('#name').val();
-  const comment = $('#comment').val();
-
-  $.ajax({
-    type: 'POST',
-    url: '/send',
-    contentType: 'application/json',
-    data: JSON.stringify({
-      name: name,
-      comment: comment
-    }),
-    success: (data)=>{
-      console.log('Success Text: ', data);
-    }
-  });
-
-});
-
-//Display text on browser
-const getTextFile = () =>{
-  $.get('/book.txt',(data) =>{
-    const display = $('#browserViewText');     
-    display.html(data);
-    }
-  );
-}
-
-window.setInterval(getTextFile, 1000);
-
 //video 
 const video = $('#video')[0];
-
-
 navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream)=>{
   video.srcObject = stream;
   video.play();
